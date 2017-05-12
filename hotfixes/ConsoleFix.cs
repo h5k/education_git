d603c5005678574fff064b530984fd74584b12fd
606 <hwk@ukr.net>
2017-05-13 02:13:39 +0300

    Added ConsoleFix.cs for 0.01 RELEASE

---
+++ hotfixes/ConsoleFix.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace GitConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, git :3");
+            Console.ReadKey();
+        }
+    }
+}
